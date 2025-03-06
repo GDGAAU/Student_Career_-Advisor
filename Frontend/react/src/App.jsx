cc9013e615d40237ab63a21fba970665eb12c3f9
=elijahizkiel <=>
2025-03-06 21:02:56 +0300

    fix onchange bug at confirm password

--- Frontend/react/src/App.jsx
+++ Frontend/react/src/App.jsx
@@ -7,20 +7,21 @@ import Header from "./components/Header.jsx";
 import FormCareerRequest from "./components/FormCareerRequest.jsx";
 import User from "./components/User.jsx";
 import Footer from "./components/Footer.jsx";
+import { LoginProvider } from "./contexts/LoginContext.jsx";
 
 function App() {
   return (
-    
+    <LoginProvider>
     <Router>
       <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<User />} />
         <Route path="/form" element={<FormCareerRequest />} /> 
-        
       </Routes>
       <Footer/>
     </Router>
+    </LoginProvider>
   );
 }
 
