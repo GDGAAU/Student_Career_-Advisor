300af562dc0443249c9a816e54dfa28f3d75d305
=elijahizkiel <=>
2025-03-06 22:34:14 +0300

    create chat context to manage chatroom states

---
+++ Frontend/react/src/contexts/ChatContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState } from "react";
+
+const ChatContext = createContext();
+function ChatProvider({ children, initialChat }) {
+  let [chats, setChat] = useState(initialChat);
+  let [loadingMessage, setLoadingMessage] = useState(false)
+
+  function addMessageToChatRoom(message){
+    setChat((chat) => {
+      return chat ? [...chat, message] : [message];
+    });
+    setLoadingMessage(true)
+  }  
+
+  async function sendMessage(message,token) {
+    let msg;
+    let response;
+    if(message.trim() !== ""){
+      msg = { role: "user", parts: message.trim() };
+      addMessageToChatRoom(msg);
+
+      response = await fetch("/chat-service", {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+         "Authorization":` Bearer ${token} `
+       },
+      body: JSON.stringify(msg),
+    }).then(response => response.json());
+  }
+    if(response){
+      setChat(chat => {
+      setLoadingMessage(false)      
+      return [...chat,response];
+      });
+    }
+  }
+  return (
+    <ChatContext.Provider value={{ loadingMessage, chats, sendMessage }}>
+      {children}
+    </ChatContext.Provider>
+  );
+}
+
+export { ChatContext, ChatProvider };
