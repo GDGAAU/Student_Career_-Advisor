faf08a01e61dede0669ecd6187720c88d7f7f524
=elijahizkiel <=>
2025-03-06 22:50:45 +0300

    configure database

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from api_loader import DB_uri
+import os
+
 
 app = Flask(__name__, static_folder="../Frontend/react/build")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_uri 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 
 
 db = SQLAlchemy(app)
