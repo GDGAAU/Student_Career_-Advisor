b1632558889c51f163b872ce2ca5928b82a67172
rita <ritaayalew6@gmail.com>
2025-03-06 14:06:11 +0300

    build career preference form

---
+++ Frontend/react/src/components/FormCareerRequest.jsx
@@ -0,0 +1,217 @@
+import React, { useState } from "react";
+import './form.css'; // Ensure this CSS file is linked for styles
+
+const FormCareerRequest = () => {
+  const [showInput, setShowInput] = useState(false);
+  const [inputs, setInputs] = useState([{ id: Date.now(), value: "" }]);
+  const [hobbies, setHobbies] = useState([{ id: Date.now(), value: "" }]);
+
+  // for special needs
+  const handleRadioChange = (event) => {
+    setShowInput(event.target.value === "yes");
+  };
+
+  const handleAddInput = () => {
+    setInputs([...inputs, { id: Date.now(), value: "" }]);
+  };
+
+  const handleRemoveInput = (id) => {
+    setInputs(inputs.filter((input) => input.id !== id));
+  };
+
+  const handleChange = (id, value) => {
+    setInputs(
+      inputs.map((input) =>
+        input.id === id ? { ...input, value: value } : input
+      )
+    );
+  };
+
+  // for hobbies
+  const handleAddHobby = () => {
+    setHobbies([...hobbies, { id: Date.now(), value: "" }]); // Correctly update hobbies state
+  };
+
+  const handleRemoveHobby = (id) => {
+    setHobbies(hobbies.filter((hobby) => hobby.id !== id)); // Correctly update hobbies state
+  };
+
+  const handleHobbyChange = (id, value) => {
+    setHobbies(
+      hobbies.map((hobby) =>
+        hobby.id === id ? { ...hobby, value: value } : hobby
+      )
+    ); // Correctly update hobbies state
+  };
+
+  return (
+    <div className="form-career-request">
+      <form>
+        <div className="form-section">
+          <div className="form-group">
+            <label htmlFor="user-role">Who are you?</label>
+            <select id="user-role" name="userRole" className="form-select">
+              <option value="university-student" className="option-style">
+                University Student
+              </option>
+              <option value="job-seeker" className="option-style">
+                Job Seeker
+              </option>
+              <option value="career-counselor" className="option-style">
+                Career Counselor
+              </option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="user-age">Age</label>
+            <input
+              type="text"
+              id="user-age"
+              placeholder="Enter your age"
+              className="form-input"
+            />
+          </div>
+
+          {/* Hobbies Section */}
+          <div className="form-group">
+            <label htmlFor="user-hobby">Hobby</label>
+            {hobbies.map((hobby) => (
+              <div key={hobby.id} className="form-group input-container">
+                <input
+                  type="text"
+                  placeholder="Please specify..."
+                  value={hobby.value}
+                  onChange={(e) => handleHobbyChange(hobby.id, e.target.value)}
+                  className="form-input"
+                />
+                <button
+                  type="button"
+                  className="button-add"
+                  onClick={handleAddHobby}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  className="button-remove"
+                  onClick={() => handleRemoveHobby(hobby.id)}
+                >
+                  x
+                </button>
+              </div>
+            ))}
+          </div>
+
+          {/* Special Needs Section */}
+          <div className="form-group">
+            <label>Do you have special needs?</label>
+            <div className="radio-group">
+              <label>
+                <input
+                  type="radio"
+                  name="special-needs"
+                  value="yes"
+                  onChange={handleRadioChange}
+                />
+                Yes
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="special-needs"
+                  value="no"
+                  onChange={handleRadioChange}
+                />
+                No
+              </label>
+            </div>
+          </div>
+
+          {showInput &&
+            inputs.map((input) => (
+              <div key={input.id} className="form-group input-container">
+                <input
+                  type="text"
+                  placeholder="Please specify..."
+                  value={input.value}
+                  onChange={(e) => handleChange(input.id, e.target.value)}
+                  className="form-input"
+                />
+                <button
+                  type="button"
+                  className="button-add"
+                  onClick={handleAddInput}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  className="button-remove"
+                  onClick={() => handleRemoveInput(input.id)}
+                >
+                  x
+                </button>
+              </div>
+            ))}
+        </div>
+
+        <div className="form-section">
+          <div className="form-group">
+            <label>Interested Job Types</label>
+            <div className="radio-group">
+              <label>
+                <input type="radio" name="job-type" value="on-site" />
+                On-site
+              </label>
+              <label>
+                <input type="radio" name="job-type" value="remote" />
+                Remote
+              </label>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="field-of-interest">Field of interest</label>
+            <input
+              type="text"
+              id="field-of-interest"
+              placeholder="Your interests here"
+              className="form-input"
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Salary Range</label>
+            <div className="input-inline">
+              <div>
+                <label htmlFor="salary-min">Min</label>
+                <input
+                  type="text"
+                  id="salary-min"
+                  className="form-input salary-input"
+                  placeholder="Minimum salary"
+                />
+              </div>
+              <div>
+                <label htmlFor="salary-max">Max</label>
+                <input
+                  type="text"
+                  id="salary-max"
+                  className="form-input salary-input"
+                  placeholder="Maximum salary"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FormCareerRequest;
+
+
+
+
