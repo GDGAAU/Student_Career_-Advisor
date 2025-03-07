47b0e625b3a7238898a54b903da4631e12073c58
rita <ritaayalew6@gmail.com>
2025-03-07 07:43:39 +0300

    modify consultancy page

--- Frontend/react/src/components/chatroom.jsx
+++ Frontend/react/src/components/chatroom.jsx
@@ -5,40 +5,40 @@ import { ChatContext, ChatProvider } from "../contexts/ChatContext";
 import BeatLoader from "react-spinners/BeatLoader";
 
 const Chatroom = () => {
-  const { token } = useContext(LoginContext);
-  const { loadingMessage, chats, sendMessage } = useContext(ChatContext);
-  const [input, setInput] = useState("");
-  const [initialChat, setInitialChat] = useState([]);
-  const handleSendMessage = (e) => {
-    e.preventDefault();
-    sendMessage(e.target.value, token);
-  };
+//   const { token } = useContext(LoginContext);
+//   const { loadingMessage, chats, sendMessage } = useContext(ChatContext);
+//   const [input, setInput] = useState("");
+//   const [initialChat, setInitialChat] = useState([]);
+//   const handleSendMessage = (e) => {
+//     e.preventDefault();
+//     sendMessage(e.target.value, token);
+//   };
 
-  useEffect(() => {
-    const fetchMessages = async () => {
-      const messages = await fetch("http://127.0.0.1/get-messages", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }).then((response) => {
-        if (response.ok) {
-          return { messages: response.json(), status: 0 };
-        } else {
-          return { messages: null, status: 1 };
-        }
-      });
-      return { messages: messages, status: messages.status };
-    };
-    const messages = fetchMessages();
-    if (messages.status == 0) {
-      setInitialChat(messages.messages);
-    }
-    return () => {
-      setInitialChat([]);
-    };
-  }, [token]);
+//   useEffect(() => {
+//     const fetchMessages = async () => {
+//       const messages = await fetch("http://127.0.0.1/get-messages", {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: `Bearer ${token}`,
+//         },
+//       }).then((response) => {
+//         if (response.ok) {
+//           return { messages: response.json(), status: 0 };
+//         } else {
+//           return { messages: null, status: 1 };
+//         }
+//       });
+//       return { messages: messages, status: messages.status };
+//     };
+//     const messages = fetchMessages();
+//     if (messages.status == 0) {
+//       setInitialChat(messages.messages);
+//     }
+//     return () => {
+//       setInitialChat([]);
+//     };
+//   }, [token]);
 
   return (
     <ChatProvider initialChat={initialChat}>
